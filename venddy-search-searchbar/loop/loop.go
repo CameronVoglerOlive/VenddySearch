321b921df5da3690f62fe632020529347110faaa
Cameron Vogler <cvogler@catalystsolves.com>
2021-03-10 10:23:31 -0800

    disambiguation is working with pagination

--- venddy-search-searchbar/loop/loop.go
+++ venddy-search-searchbar/loop/loop.go
@@ -40,7 +40,7 @@ func NewLoop(logger *ldk.Logger) (*Loop, error) {
 }
 
 var client = &http.Client{Timeout: 10 * time.Second}
-var searchLimit = 5
+var searchLimit = 10
 var cursor = 0
 
 // LoopStart is called by the host when the loop is started to provide access to the host process
@@ -58,16 +58,7 @@ func (l *Loop) LoopStart(sidekick ldk.Sidekick) error {
 		}
 
 		go func() {
-			req, err := http.NewRequest("GET",
-				fmt.Sprintf(`https://venddy.com/api/1.1/obj/vendor?constraints=[{"key":"searchfield", "constraint_type":"text contains", "value":"%v" }]&sort_field=Score&descending=true&limit=%v&cursor=%v`,
-					text, searchLimit, cursor), nil)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			q := req.URL.Query()
-			req.URL.RawQuery = q.Encode()
-
-			resp, err := client.Do(req)
+			resp, err := GetVenddySearchResults(text, searchLimit, cursor)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -85,66 +76,163 @@ func (l *Loop) LoopStart(sidekick ldk.Sidekick) error {
 				log.Fatalln(er.Error())
 			}
 
-			listElements := make(map[string]ldk.WhisperContentListElement)
-
-			if len(venddyResponse.Response.Results) > 0 {
-				for i := range venddyResponse.Response.Results {
-					isExtra := i != 0
-					listElements[fmt.Sprintf("%v", i)] = &ldk.WhisperContentListElementLink{
-						Extra: isExtra,
-						Align: ldk.WhisperContentListElementAlignLeft,
-						Href:  venddyResponse.Response.Results[i].Website,
-						Order: uint32(i),
-						Style: ldk.WhisperContentListElementStyleNone,
-						Text:  venddyResponse.Response.Results[i].Name,
-					}
-
-					listElements[fmt.Sprintf("a_%v", i)] = &ldk.WhisperContentListElementPair{
-						Label: "Rating",
-						Order: uint32(i),
-						Extra: isExtra,
-						Value: fmt.Sprintf("%v", venddyResponse.Response.Results[i].Score),
-					}
-
-					listElements[fmt.Sprintf("b_%v", i)] = &ldk.WhisperContentListElementPair{
-						Label: "Reivews",
-						Order: uint32(i),
-						Extra: isExtra,
-						Value: fmt.Sprintf("%v", venddyResponse.Response.Results[i].ReviewCount),
-					}
-
-					listElements[fmt.Sprintf("c_%v", i)] = &ldk.WhisperContentListElementMessage{
-						Style:  ldk.WhisperContentListElementStyleNone,
-						Header: venddyResponse.Response.Results[i].Keywords,
-						Body:   venddyResponse.Response.Results[i].Description,
-						Align:  ldk.WhisperContentListElementAlignLeft,
-						Order:  uint32(i),
-						Extra:  isExtra,
-					}
-
-					listElements[fmt.Sprintf("d_%v", i)] = &ldk.WhisperContentListElementDivider{
-						Order: uint32(i),
-						Extra: isExtra,
-					}
+			go func() {
+				_, err := l.sidekick.Whisper().Disambiguation(l.ctx, &ldk.WhisperContentDisambiguation{
+					Label:    "Venddy Search",
+					Elements: l.CreateDisambiguationElements(venddyResponse.Response, text),
+				})
+				if err != nil {
+					l.logger.Error("failed to emit whisper", "error", err)
 				}
-
-				err = l.sidekick.Whisper().List(l.ctx, &ldk.WhisperContentList{
-					Label:    fmt.Sprintf("Venddy Search Results for %v", text),
-					Elements: listElements,
-				})
-			} else {
-				err = l.sidekick.Whisper().Markdown(l.ctx, &ldk.WhisperContentMarkdown{
-					Label:    fmt.Sprintf("Venddy Search Results for %v", text),
-					Markdown: fmt.Sprintf("%v didn't return any results, try another seach", text),
-				})
-			}
-
-			if err != nil {
-				l.logger.Error("failed to emit whisper", "error", err)
-				return
-			}
+			}()
 		}()
 	})
+}
+
+func (l *Loop) CreateDisambiguationElements(response Response, text string) map[string]ldk.WhisperContentDisambiguationElement {
+	elements := make(map[string]ldk.WhisperContentDisambiguationElement)
+
+	for i := range response.Results {
+		item := response.Results[i]
+		elements[fmt.Sprintf("%v", i)] = &ldk.WhisperContentDisambiguationElementOption{
+			Label: item.Name,
+			Order: uint32(i) + 1,
+			OnChange: func(key string) {
+				go func() {
+					err := l.sidekick.Whisper().List(l.ctx, &ldk.WhisperContentList{
+						Label:    item.Name,
+						Elements: CreateListElements(item),
+					})
+					if err != nil {
+						log.Fatalln(err)
+					}
+				}()
+			},
+		}
+	}
+
+	elements["header1"] = &ldk.WhisperContentDisambiguationElementText{
+		Body:  fmt.Sprintf("# Results for %v:", text),
+		Order: 0,
+	}
+	elements["header2"] = &ldk.WhisperContentDisambiguationElementText{
+		Body:  fmt.Sprintf("Remaining Results: %v", response.Remaining),
+		Order: uint32(len(response.Results)) + 2,
+	}
+
+	if response.Remaining > 0 {
+		elements["next"] = &ldk.WhisperContentDisambiguationElementOption{
+			Label: fmt.Sprintf("next %v results", searchLimit),
+			Order: uint32(len(response.Results)) + 3,
+			OnChange: func(key string) {
+				go func() {
+					cursor += searchLimit
+					resp, err := GetVenddySearchResults(text, searchLimit, cursor)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					defer resp.Body.Close()
+
+					bodyBytes, err := ioutil.ReadAll(resp.Body)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					venddyResponse := VenddyResponse{}
+
+					er := json.Unmarshal(bodyBytes, &venddyResponse)
+					if er != nil {
+						log.Fatalln(er.Error())
+					}
+					_, _ = l.sidekick.Whisper().Disambiguation(l.ctx, &ldk.WhisperContentDisambiguation{
+						Label:    "Venddy Search",
+						Elements: l.CreateDisambiguationElements(venddyResponse.Response, text),
+					})
+				}()
+			},
+		}
+	}
+
+	if cursor > 0 {
+		elements["prev"] = &ldk.WhisperContentDisambiguationElementOption{
+			Label: fmt.Sprintf("prev %v results", searchLimit),
+			Order: uint32(len(response.Results)) + 4,
+			OnChange: func(key string) {
+				go func() {
+					cursor -= searchLimit
+					resp, err := GetVenddySearchResults(text, searchLimit, cursor)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					defer resp.Body.Close()
+
+					bodyBytes, err := ioutil.ReadAll(resp.Body)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					venddyResponse := VenddyResponse{}
+
+					er := json.Unmarshal(bodyBytes, &venddyResponse)
+					if er != nil {
+						log.Fatalln(er.Error())
+					}
+					_, _ = l.sidekick.Whisper().Disambiguation(l.ctx, &ldk.WhisperContentDisambiguation{
+						Label:    "Venddy Search",
+						Elements: l.CreateDisambiguationElements(venddyResponse.Response, text),
+					})
+				}()
+			},
+		}
+	}
+
+	return elements
+}
+
+func CreateListElements(result VenddyResult) map[string]ldk.WhisperContentListElement {
+	elements := map[string]ldk.WhisperContentListElement{}
+	elements[fmt.Sprintf("%v_website", result.Id)] = &ldk.WhisperContentListElementLink{
+		Align: ldk.WhisperContentListElementAlignLeft,
+		Href:  result.Website,
+		Order: 0,
+		Style: ldk.WhisperContentListElementStyleNone,
+		Text:  result.Website,
+	}
+
+	elements[fmt.Sprintf("%v_rating", result.Id)] = &ldk.WhisperContentListElementPair{
+		Label: "Rating",
+		Order: 1,
+		Value: fmt.Sprintf("%v", result.Score),
+	}
+
+	elements[fmt.Sprintf("%v_reviews", result.Id)] = &ldk.WhisperContentListElementPair{
+		Label: "Reivews",
+		Order: 2,
+		Value: fmt.Sprintf("%v", result.ReviewCount),
+	}
+
+	elements[fmt.Sprintf("%v_description", result.Id)] = &ldk.WhisperContentListElementMessage{
+		Style:  ldk.WhisperContentListElementStyleNone,
+		Header: result.Description,
+		Body:   result.Keywords,
+		Align:  ldk.WhisperContentListElementAlignLeft,
+		Order:  3,
+	}
+
+	return elements
+}
+
+func GetVenddySearchResults(text string, max int, startAt int) (*http.Response, error) {
+	req, err := http.NewRequest("GET",
+		fmt.Sprintf(`https://venddy.com/api/1.1/obj/vendor?constraints=[{"key":"searchfield", "constraint_type":"text contains", "value":"%v" }]&sort_field=Score&descending=true&limit=%v&cursor=%v`,
+			text, max, startAt), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	q := req.URL.Query()
+	req.URL.RawQuery = q.Encode()
+
+	return client.Do(req)
 }
 
 type VenddyResponse struct {
@@ -152,13 +240,14 @@ type VenddyResponse struct {
 }
 
 type Response struct {
-	Results   []VenddyResults `json:"results"`
-	Cursor    int             `json:"Cursor"`
-	Remaining int             `json:"Remaining"`
-	Count     int             `json:"Count"`
+	Results   []VenddyResult `json:"results"`
+	Cursor    int            `json:"Cursor"`
+	Remaining int            `json:"Remaining"`
+	Count     int            `json:"Count"`
 }
 
-type VenddyResults struct {
+type VenddyResult struct {
+	Id          string  `json:"_id"`
 	Website     string  `json:"Website"`
 	Description string  `json:"Description"`
 	Name        string  `json:"Name"`
